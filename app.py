85d59da798f70149b5de38ab0b93f14be653b6a5
Jose Garayar <jgarayarp@uni.pe>
2024-05-04 11:24:15 -0500

    Add javascript validation to select field in form, change result value

--- app.py
+++ app.py
@@ -16,26 +16,26 @@ def index():
 
 @app.route("/predict",methods=['POST'])
 def predict():
-    gender = request.form['Gender']
-    age = request.form['Age']
+    gender = request.form['gender']
+    age = request.form['age']
     annual_income = request.form['annualIncome']
     spending_score = request.form['spendingScore']
 
     feature_list = ([[gender, age, annual_income, spending_score]])
-    # single_pred = np.array(feature_list).reshape(1, -1)
 
     transformed_features = ct.transform(feature_list)
     transformed_features[:,2:] = sc.transform(transformed_features[:,2:])
     prediction = model.predict(transformed_features).reshape(1,-1)
 
-    # diccionario = {1: "Cluster 1", 2: "Cluster 2", 3: "Cluster 3", 4: "Cluster 4", 5: "Cluster 5"}
+    diccionario = {0: "Cluster 1", 1: "Cluster 2", 2: "Cluster 3", 3: "Cluster 4", 4: "Cluster 5"}
 
-    # if prediction[0] in diccionario:
-    #     crop = diccionario[prediction[0]]
-    #     result =("Lo mejor para plantar es: {} ".format(crop))
-    # else:
-    #     result =("Sorry, Hoy no se come")
-    return render_template('index.html',result = prediction[0])
+    value_predicted = prediction[0][0]
+    if value_predicted in diccionario:
+        crop = diccionario[value_predicted]
+        result =("Cliente pertenece a {} ".format(crop))
+    else:
+        result =("Error! No se puede agrupar al cliente")
+    return render_template('index.html',result = result)
 
 
 
